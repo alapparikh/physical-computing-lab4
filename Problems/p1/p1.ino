f2ca9fd5fb799aad80f4d12dad313eb4cc532f88
Harrison Gregg <animasterh@gmail.com>
2015-11-06 12:21:29 -0500

    1 and 2 working. 3 in progress.

--- Problems/p1/p1.ino
+++ Problems/p1/p1.ino
@@ -2,16 +2,16 @@
  Time Waster Wars: Reddit vs. 4chan
 
  a. Add synchronization primitives to ensure that
-    (a) the club is exclusively redditors or 4channers, i.e. no redditors
-        should enter as long as there are 4channers in the club,
-        and vice versa,
-    (b) the club should always be used as long as there are
-        customers
+		(a) the club is exclusively redditors or 4channers, i.e. no redditors
+				should enter as long as there are 4channers in the club,
+				and vice versa,
+		(b) the club should always be used as long as there are
+				customers
 
-    Note that starvation is not something you need to worry
-    about. If the club becomes redditors and remains exclusively
-    redditors for all time, the waiting 4channers will just have
-    to get old at the door.
+		Note that starvation is not something you need to worry
+		about. If the club becomes redditors and remains exclusively
+		redditors for all time, the waiting 4channers will just have
+		to get old at the door.
 
  Modify only the code of the class Club to make the program
  correct. (but add the LED manipulation to the other classes)
@@ -23,177 +23,193 @@
 
 
 void hang_out() {
-  delay(random(300,1500)); //waste time
+	delay(random(300,1500)); //waste time
 }
 
 class Club {
-  
-  /* We have declared one lock and one condition
-   * variable here.  You may (or may not) find it
-   * useful to declare additional condition variables
-   * to solve the problem.  We just left these here as
-   * a reminder on the syntax. */
-  Lock *_l;
-  Cond *_cr;
-  Cond *_c4; 
+	
+	/* We have declared one lock and one condition
+	 * variable here.  You may (or may not) find it
+	 * useful to declare additional condition variables
+	 * to solve the problem.  We just left these here as
+	 * a reminder on the syntax. */
+	Lock *_l;
+	Cond *_cr; 
+	Cond *_c4; 
 
-  int num_redditors;
-  int num_4channers;
+	int num_redditors;
+	int num_4channers;
 
-  public:
-    Club () {  //Constructor for club
-      _l = new Lock();
-      _cr = new Cond(_l);
-      _c4 = new Cond(_l);
-    }
+	public:
+		Club () {  //Constructor for club
+			_l = new Lock();
+			_cr = new Cond(_l);
+			_c4 = new Cond(_l);
+			num_redditors = 0;
+			num_4channers = 0;
+		}
 
-    ~Club () { //Destructor for club
-      delete _l;
-      delete _cr;
-      delete _c4;
-    }
-    
-    void redditor_enter() {
-      /*TODO: make it so that this function only returns when it is safe for
-       *      redditor to enter the club. */
-      Serial.println("Redditor trying to enter");
-       
-      _l->lock();
-      if (num_4channers > 0) {
-        _l->unlock();
-        _cr->wait();
-      }
+		~Club () { //Destructor for club
+			delete _l;
+			delete _cr;
+			delete _c4;
+		}
 
-      num_redditors += 1;
+		void reinit() {
+			// This shouldn't be necessary, but it doesn't work without it
+			_l = new Lock();
+			_cr = new Cond(_l);
+			_c4 = new Cond(_l);
+		}
+		
+		void redditor_enter() {
+			/*TODO: make it so that this function only returns when it is safe for
+			 *      redditor to enter the club. */
 
-      if (_cr->waiting()) {
-        _cr->signal();
-      }
-      else {
-        _l->unlock();
-      }
-    }
+			_l->lock();
+			if(num_4channers > 0) {
+				_l->unlock();
+				_cr->wait();
+			}
+			num_redditors++;
+			if(_cr->waiting()) {
+				_cr->signal();
+			} else {
+				_l->unlock();
+			}
+		}
 
-    void redditor_exit() {
-      /*TODO: Exit the club. */
+		void redditor_exit() {
+			/*TODO: Exit the club. */
+			_l->lock();
+			num_redditors--;
+			if(num_redditors > 0){
+				_l->unlock();
+			} else {
+				if(_c4->waiting()){
+					_c4->signal();
+				} else {
+					_l->unlock();
+				}
+			}
+		}
 
-      _l->lock();
-      num_redditors -= 1;
+		void fourchanner_enter() {
+			/*TODO: make it so that this function only returns when it is safe for
+			 *      redditor to enter the club. */
+			_l->lock();
+			if(num_redditors > 0) {
+				_l->unlock();
+				_c4->wait();
+			}
+			num_4channers++;
+			if(_c4->waiting()) {
+				_c4->signal();
+			} else {
+				_l->unlock();
+			}
+		}
 
-      if (num_redditors > 0) {
-        _l->unlock();
-      }
-      else {
-        if (_c4->waiting()) {
-          _c4->signal();
-        }
-        else {
-          _l->unlock();
-        }
-      }
-    }
-
-    void fourchanner_enter() {
-      /*TODO: make it so that this function only returns when it is safe for
-       *      redditor to enter the club. */
-
-      _l->lock();
-      if (num_redditors > 0) {
-        _l->unlock();
-        _c4->wait();
-      }
-
-      num_4channers += 1;
-
-      if (_c4->waiting()) {
-        _c4->signal();
-      }
-      else {
-        _l->unlock();
-      }
-    }
-
-    void fourchanner_exit() {
-      /*TODO: Exit the club. */
-
-      _l->lock();
-      num_4channers -= 1;
-
-      if (num_4channers > 0) {
-        _l->unlock();
-      }
-      else {
-        if (_cr->waiting()) {
-          _cr->signal();
-        }
-        else {
-          _l->unlock();
-        }
-      }
-    }
+		void fourchanner_exit() {
+			/*TODO: Exit the club. */
+			_l->lock();
+			num_4channers--;
+			if(num_4channers > 0){
+				_l->unlock();
+			} else {
+				if(_cr->waiting()){
+					 _cr->signal();
+				} else {
+					_l->unlock();
+				}
+		 	}
+		}
 };
 
 Club daclub;
+int columns [5] = {0,0,0,0,0};
+
+void printColumns() {
+	for(int i = 0; i < 5; i++){
+		Serial.print(columns[i]);
+	}
+	Serial.println();
+}
 
 class Redditor: Process {
-  int _id;
+	int _id;
 
 public:
-  Redditor (int id) { 
-    _id = id; 
-  }
+	Redditor (int id) { 
+		_id = id; 
+	}
 
-  void loop () {
-  	daclub.redditor_enter();
-    Serial.print("Redditor ");
-    Serial.print(_id);
-    Serial.println(": in the club");
-    /* TODO: light up column #_id on the LED matrix */
-    hang_out();
-    daclub.redditor_exit();
-  }
+	void loop () {
+		daclub.redditor_enter();
+		Serial.print("Redditor ");
+		Serial.print(_id);
+		Serial.println(": in the club");
+		/* TODO: light up column #_id on the LED matrix */
+		columns[_id-1] = 1;
+		Serial.print("1a");
+		printColumns();
+		hang_out();
+		Serial.print("1b");
+		columns[_id-1] = 0;
+		printColumns();
+		daclub.redditor_exit();
+	}
 };
 
 class Fourchanner: Process {
-  int _id;
+	int _id;
 
 public:
-  Fourchanner (int id) { 
-    _id = id; 
-  }
+	Fourchanner (int id) { 
+		_id = id; 
+	}
 
-  void loop () {
-  	daclub.fourchanner_enter();
-    Serial.print("Fourchanner ");
-    Serial.print(_id);
-    Serial.println(": in the club");
-    /* TODO: light up column #_id on the LED matrix */
-    hang_out();
-    daclub.fourchanner_exit();
-  }
+	void loop () {
+		daclub.fourchanner_enter();
+		Serial.print("Fourchanner ");
+		Serial.print(_id);
+		Serial.println(": in the club");
+		/* TODO: light up column #_id on the LED matrix */
+		columns[_id-1] = 1;
+		Serial.print("2a");
+		printColumns();
+		hang_out();
+		columns[_id-1] = 0;
+		Serial.print("2b");
+		printColumns();
+		daclub.fourchanner_exit();
+	}
 };
 
 
 // the setup routine runs once when you press reset:
 void setup() {                
 
-//  Redditor *r;
-//  Fourchanner *f;
-  
-  Serial.begin(9600); // open serial terminal
-//  Process::Init();  // start the threading library
+	Redditor *r;
+	Fourchanner *f;
+	
+	Serial.begin(9600); // open serial terminal
+	Serial.flush();
 
-  Serial.println("Hello world");
-  
-//  daclub = Club();
-//  r = new Redditor(1); //start first thread
-//  r = new Redditor(2); //start second thread
-//  f = new Fourchanner(4); //start third thread
-//  f = new Fourchanner(5); //start fourth thread
+	Process::Init();  // start the threading library
+
+	daclub = Club();
+	daclub.reinit();
+
+	r = new Redditor(1); //start first thread
+	r = new Redditor(2); //start second thread
+	r = new Redditor(3); //start second thread
+	f = new Fourchanner(4); //start third thread
+	f = new Fourchanner(5); //start fourth thread
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-//  Process::Start();
+	Process::Start();
 }
 
--- Problems/p3/p3.ino
+++ Problems/p3/p3.ino
@@ -21,154 +21,176 @@
 
 class Barrier {
 
-    /* We have declared one lock and one condition
-     * variable here.  You may (or may not) find it
-     * useful to declare additional condition variables
-     * to solve the problem.  We just left these here as
-     * a reminder on the syntax. */
-    Lock* _l;
-    Cond* _c;
-    int _n;
+		/* We have declared one lock and one condition
+		 * variable here.  You may (or may not) find it
+		 * useful to declare additional condition variables
+		 * to solve the problem.  We just left these here as
+		 * a reminder on the syntax. */
+		Lock* _l;
+		Cond* _c;
+		int _n;
+		int waiting;
 
-  public:
-    Barrier (int n) {  //Constructor for club
-      _l = new Lock();
-      _c = new Cond(_l);
-      _n = n;
-    }
+	public:
+		Barrier (int n) {  //Constructor for club
+			_l = new Lock();
+			_c = new Cond(_l);
+			_n = n;
+			waiting = 0;
+		}
 
-    void wait() {
-      /*TODO: Only return when _n other threads have also called wait*/
-    }
+		void reinit() {
+			_l = new Lock();
+			_c = new Cond(_l);
+		}
+		
+		void wait() {
+			/*TODO: Only return when _n other threads have also called wait*/
+			_l->lock();
+			if (waiting < _n-1){
+				waiting++;
+				_l->unlock();
+				_c->wait();
+				waiting--;
+			} 
+			if (waiting > 0){
+				_c->signal();
+			} else {
+				_l->unlock();
+			}
+		}
 };
 
 Barrier B(3);
 
 class CalcThread : Process {
-  public:
-  virtual int get_num();
+	public:
+	virtual int get_num();
 };
 
 CalcThread* threads [3];
 
 class Sum : CalcThread {
 
-  int _id;
-  int _iteration;
-  int _num;
-  
-  public:
-    Sum (int id) {
-      _id = id;
-      _iteration = 0;
-      _num = 0;
-    }
+	int _id;
+	int _iteration;
+	int _num;
+	
+	public:
+		Sum (int id) {
+			_id = id;
+			_iteration = 0;
+			_num = 0;
+		}
 
-    int get_num() {
-      return _num;
-    }
-    void loop () {
-      delay(random(300, 1500)); //do some computation
-      _iteration++;
-      _num = random(1,100);
-      /* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
-      B.wait();
+		int get_num() {
+			return _num;
+		}
 
-      int output = 0;
-      for (int i = 0; i < 3; ++i)
-        output += threads[i]->get_num();
+		void loop () {
+			delay(random(300, 1500)); //do some computation
+			_iteration++;
+			_num = random(1,100);
+			/* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
+			B.wait();
 
-      Serial.print("Sum = ");
-      Serial.println(output);
+			int output = 0;
+			for (int i = 0; i < 3; ++i)
+				output += threads[i]->get_num();
 
-      B.wait();
-    }
+			Serial.print("Sum = ");
+			Serial.println(output);
+
+			B.wait();
+		}
 };
 
 class Mean : CalcThread {
 
-  int _id;
-  int _iteration;
-  int _num;
-  
-  public:
-    Mean (int id) {
-      _id = id;
-      _iteration = 0;
-      _num = 0;
-    }
+	int _id;
+	int _iteration;
+	int _num;
+	
+	public:
+		Mean (int id) {
+			_id = id;
+			_iteration = 0;
+			_num = 0;
+		}
 
-    int get_num() {
-      return _num;
-    }
-    void loop () {
-      delay(random(300, 1500)); //do some computation
-      _iteration++;
-      _num = random(1, 100);
-      /* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
-      B.wait();
+		int get_num() {
+			return _num;
+		}
+		void loop () {
+			delay(random(300, 1500)); //do some computation
+			_iteration++;
+			_num = random(1, 100);
+			/* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
+			B.wait();
 
-      float output = 0.0;
-      for (int i = 0; i < 3; ++i)
-        output += threads[i]->get_num()/3.0;
-      
-      Serial.print("Mean = ");
-      Serial.println(output);
+			float output = 0.0;
+			for (int i = 0; i < 3; ++i)
+				output += threads[i]->get_num()/3.0;
+			
+			Serial.print("Mean = ");
+			Serial.println(output);
 
-      B.wait();
-    }
+			B.wait();
+		}
 };
 
 class Printer : CalcThread {
 
-  int _id;
-  int _iteration;
-  int _num;
-  
-  public:
-    Printer (int id) {
-      _id = id;
-      _iteration = 0;
-      _num = 0;
-    }
+	int _id;
+	int _iteration;
+	int _num;
+	
+	public:
+		Printer (int id) {
+			_id = id;
+			_iteration = 0;
+			_num = 0;
+		}
 
-    int get_num() {
-      return _num;
-    }
-    void loop () {
-      delay(random(300, 1500)); //do some computation
-      _iteration++;
-      _num = random(300, 1500);
-      /* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
-      B.wait();
+		int get_num() {
+			return _num;
+		}
+		void loop () {
+			delay(random(300, 1500)); //do some computation
+			_iteration++;
+			_num = random(300, 1500);
+			/* TODO: Make the pixel at (_id, _iteration) of the LED matrix light up (and stay lit)*/
+			B.wait();
 
-      Serial.print("Numbers: [");
-      for (int i = 0; i < 3; ++i) {
-        Serial.print(threads[i]->get_num());
-      }
-      Serial.println("]");
+			Serial.print("Numbers: [");
+			for (int i = 0; i < 3; ++i) {
+				Serial.print(threads[i]->get_num());
+			}
+			Serial.println("]");
 
-      B.wait();
-    }
+			B.wait();
+		}
 };
 
 // the setup routine runs once when you press reset:
 void setup() {
+	Sum *s;
+	Mean *m;
+	Printer *p;
+	
+	Serial.begin(9600); // open serial terminal
+	Process::Init();  // start the threading library
+	Serial.flush();
+	
+	B.reinit();
 
-  Sum *s;
-  Mean *m;
-  Printer *p;
-  
-  Serial.begin(9600); // open serial terminal
-  Process::Init();  // start the threading library
-
-  s = new Sum(1); //start first thread
-  m = new Mean(2); //start second thread
-  p = new Printer(3); //start third thread
+	s = new Sum(1); //start first thread
+	m = new Mean(2); //start second thread
+	p = new Printer(3); //start third thread
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  Process::Start();
+	Process::Start();
 }
 
